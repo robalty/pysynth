c9aa95767bdcd999edad5d44aa4541e0f5a8ad4c
sciwizard <audio.sauce@gmail.com>
2020-05-08 10:46:34 -0700

    added inst selector

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGridLayout
+from PyQt5.QtWidgets import QComboBox
 from PyQt5.QtGui import QIcon, QPixmap
 
 class App(QWidget):
@@ -25,9 +26,14 @@ class App(QWidget):
         label.setPixmap(pixmap)
         self.resize(pixmap.width(), pixmap.height())
 
+        # Create instrument selector
+        inst_select = QComboBox()
+        inst_select.addItem('Synth 1')
+        inst_select.addItem('Senth 2')
 
         # Set up the widget grid layout
         layout.addWidget(label, 0, 0)
+        layout.addWidget(inst_select, 1, 0)
         self.setLayout(layout)
         
         self.show()
