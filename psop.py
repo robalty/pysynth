ed24bccbc553a24765698d8f7a8916c66605afc1
robalty <32559243+robalty@users.noreply.github.com>
2020-05-13 15:11:43 -0700

    Changed backend
    I'll have to reimplement the unique algorithms, but I changed the backend to use vectorization when getting samples, which should be a lot faster.

--- psop.py
+++ psop.py
@@ -3,40 +3,38 @@ import numpy as np
 
 SAMPLERATE = 48000
 
-SINETABLE = np.sin(2 * np.pi * np.arange(0, 1, 1/48000))
-
-
 class ADSR:
-    def __init__(self, a=0.1, d1=0.3, d2=1, s=0.6, r=0.1, rtime=0):
-        self.timings = {
-                0 : 0,
-                1 : 1 / (4 * a * SAMPLERATE),
-                2 : -1 / (4 * d1 * SAMPLERATE),
-                3 : -1 / (4 * d2 * SAMPLERATE),
-                4 : -1 / (4 * r * SAMPLERATE)
-        }
+    def __init__(self, a=0.5, d=0.5, s=0.75, r=0.5):
+        self.a = a
+        self.d = d
         self.s = s
-        self.stage = 0
-        self.cur = 0
+        self.r = r
+        self.rvol = 0
+        self.rtime = 0
 
-    def get_vol(self):
-        if self.stage == 0:
-            return 0
-        temp = self.cur
-        self.cur += self.timings.get(self.stage)
-        if self.cur > 1:
-            self.cur = 1
-            self.stage = 2
-        elif (self.stage == 2) & (self.cur < self.s):
-            self.stage = 3
-        elif (self.cur < 0):
-            self.cur = 0
-            self.stage = 0
+    def get_vol(self, clock):
+        if self.rtime != 0:
+            return lerp(self.rvol, 0, clock - self.rtime, self.r)
+        if clock < self.a:
+            t = lerp(0, 1, clock, self.a)
+            self.rvol = t
+            return t
+        elif clock < (self.a + self.d):
+            t = lerp(1, self.s, clock, self.a + self.d)
+            self.rvol = t
+            return t
+        else:
+            return self.s
+
+    def get_vols(self, clock, num):
+        temp = []
+        for i in range(num):
+            temp.append(self.get_vol(clock + (i/SAMPLERATE)))
         return temp
 
 class Operator:
     def __init__(self, i=SAMPLERATE, f=220, m=1):
-        self.modulation = i / (2 * math.pi)
+        self.mod = 1
         self.frequency = f
         self.freq_mult = m
         self.envelope = ADSR()
@@ -44,115 +42,76 @@ class Operator:
         self.acc_phase = 0
 
     def sample(self, clock):
-        vol = self.envelope.get_vol()
-        cur = int((self.frequency * clock) + self.acc_phase) % SAMPLERATE
-        return vol * SINETABLE[cur]
+        return (self.frequency * clock) + self.acc_phase
 
     def sample_with(self, in_op, clock):
-        vol = self.envelope.get_vol()
-        cur = int((self.frequency * clock) + (in_op * self.modulation) + self.acc_phase) % SAMPLERATE
-        return vol * SINETABLE[cur]
-    
-    def sample_fb(self, clock):
-        vol = self.envelope.get_vol()
-        cur = int((self.frequency * clock) + (self.feedback * self.modulation) + self.acc_phase) % SAMPLERATE
-        ret = vol * SINETABLE[cur]
-        self.feedback = ret
-        return ret
-
+        return (self.frequency * clock) + (self.mod * in_op) + self.acc_phase
+   
 class Synth:
     def __init__(self):
         self.ops = [Operator(), Operator(), Operator(), Operator()]
         self.algorithm = 1
         self.clock = 0
-        self.samplerate = SAMPLERATE
         self.frequency = 220
         self.vol = 0
 
     def set_freq(self, val):
         self.frequency = val
         for i in self.ops:
-            t = val * i.freq_mult
-            i.acc_phase += ((i.frequency - t) * (self.clock)) % SAMPLERATE
+            t = val * i.freq_mult * 2 * np.pi
+            i.acc_phase += ((i.frequency - t) * (self.clock))
             i.frequency = t
-
-
-    def get_sample(self):
-        func = alg.get(self.algorithm)
-        return self.vol * func(self.ops, self.clock)
-
-    def step(self):
-        self.clock += 1
+    
+    def set_mod(self, val, op):
+        self.ops[op].mod = val
 
     def get_samples(self, num_samples):
-        func = alg.get(self.algorithm)
-        iterable = (self.vol * func(self.ops, x) for x in range(self.clock, self.clock+num_samples))
-        temp = np.fromiter(iterable, np.float32, num_samples)
-        self.clock += num_samples
-        self.clock = self.clock % SAMPLERATE
-        return temp
+        temp = algtest(self.ops, self.clock, num_samples) 
+        self.clock += num_samples / SAMPLERATE
+        return temp * self.vol
 
     def release(self):
         for op in self.ops:
-            op.envelope.stage = 4
+            op.envelope.rtime = self.clock
 
     def press(self):
         self.clock = 0
         for op in self.ops:
-            op.envelope.stage = 1
-            op.envelope.cur = 0
+            op.feedback = 0
+            op.envelope.rvol = 0
+            op.envelope.rtime = 0
             op.acc_phase = 0
 
 
-def algtest(ops, clock):
-    return ops[0].sample_fb(clock)
-def alg1(ops, clock):
-    first = ops[0].sample_fb(clock)
-    second = ops[1].sample_with(first, clock)
-    third = ops[2].sample_with(second, clock)
-    return ops[2].sample_with(third, clock)
-def alg2(ops, clock):
-    first = ops[0].sample_fb(clock) + ops[1].sample(clock)
-    second = ops[2].sample_with(first, clock)
-    return ops[3].sample_with(second, clock)
-def alg3(ops, clock):
-    first = ops[0].sample_fb(clock)
-    second = ops[1].sample(clock)
-    third = ops[2].sample_with(second, clock)
-    return ops[3].sample_with((first + third) / 2, clock)
-#alg4 is the same as alg3 except with (0 + 1) + 2 instead of 0 + (1 + 2)
-#this makes a difference because operator 0 can have feedback
-def alg4(ops, clock):
-    first = ops[0].sample_fb(clock)
-    second = ops[1].sample_with(first, clock)
-    third = ops[2].sample(clock)
-    return ops[3].sample_with((second + third) / 2, clock)
-def alg5(ops, clock):
-    first = ops[0].sample_fb(clock)
-    second = ops[1].sample_with(first, clock)
-    third = ops[2].sample(clock)
-    return (ops[3].sample_with(third, clock) + second) / 2
-def alg6(ops, clock):
-    first = ops[0].sample_fb(clock)
-    return (ops[1].sample_with(first, clock) + ops[2].sample_with(first, clock) + ops[3].sample_with(first, clock)) / 3
-def alg7(ops, clock):
-    first = ops[0].sample_fb(clock)
-    return (ops[1].sample_with(first, clock) + ops[2].sample(clock) + ops[3].sample(clock)) / 3
-def alg8(ops, clock):
-    return (ops[0].sample_fb(clock) + ops[1].sample(clock) + ops[2].sample(clock) + ops[3].sample(clock)) / 4
+def algtest(ops, clock, size):
+    return alg1(ops, clock, size)
 
+def samples(op, clock, size):
+    first = np.sin(np.fromfunction(lambda x: op.sample(clock + (x / SAMPLERATE)), (size,)))
+    vol = op.envelope.get_vols(clock, size)
+    return np.multiply(vol, np.sin(first))
 
-alg = {
-        0 : algtest,
-        1 : alg1,
-        2 : alg2,
-        3 : alg3,
-        4 : alg4,
-        5 : alg5,
-        6 : alg6,
-        7 : alg7,
-        8 : alg8
-}
+def samples_with(op, clock, size, in_op):
+    first = np.fromfunction(lambda x: op.sample(clock + (x / SAMPLERATE)), (size,))
+    vol = op.envelope.get_vols(clock, size)
+    s_in = np.multiply(in_op, op.mod)
+    return np.multiply(vol, np.sin(first + s_in))
+
+def samples_fb(op, clock, size):
+    temp = np.empty(size)
+    temp[0] = np.sin(op.sample(clock) + (op.mod * op.feedback))
+    for i in range(1, size):
+        temp[i] = np.sin((op.mod * temp[i-1]) + op.sample(clock + (i / SAMPLERATE)))
+    op.feedback = temp[size - 1]
+    tempvols = op.envelope.get_vols(clock, size)
+    return np.multiply(temp, tempvols)
+
+def alg1(ops, clock, size):
+    first = samples_fb(ops[0], clock, size)
+    second = samples_with(ops[1], clock, size, first)
+    third = samples_with(ops[2], clock, size, second)
+    return samples_with(ops[3], clock, size, third)
+
 
 def lerp(startval, endval, time, endtime):
     if time > endtime:
--- pysynth.py
+++ pysynth.py
@@ -1,72 +1,94 @@
 from psop import *
+from gui import *
+from PyQt5.QtCore import *
 import pyaudio
 import numpy as np
 import mido
-
-instrument = Synth()
-p = pyaudio.PyAudio()
-mido.set_backend('mido.backends.rtmidi/LINUX_ALSA')
+import sys
 
 
-def callback(in_data, frame_count, time_info, status):
-    data = np.asarray(instrument.get_samples(frame_count), dtype=np.int16)
-    return (data, pyaudio.paContinue)
-    
-stream = p.open(format=pyaudio.paInt16,
-                channels=1,
-                rate=48000,
-                output=True,
-                stream_callback=callback)
-stream.start_stream()
+class Signaller(QObject):
+    signaller = pyqtSignal()
 
-inport = mido.open_input(mido.get_input_names()[0])
 
-#VARIABLES:
-cur_note = 0
-cur_freq = 0
-cur_vol = 0
-cur_pitch = 1
-global_vol = 128
-for msg in inport:
-    if msg.type == 'note_on':
-        cur_note = msg.note
-        instrument.press()
-        cur_freq = (2 ** ((cur_note - 69) / 12)) * 440
-        instrument.set_freq(cur_freq * cur_pitch)
-        cur_vol = msg.velocity
-        instrument.vol = cur_vol * global_vol
-    elif msg.type == 'note_off':
-        if msg.note == cur_note:
-            instrument.release()
-    elif msg.type == 'pitchwheel':
-        #pitch bend - 1 octave range
-        cur_pitch = 2 ** (msg.pitch / 8192)
-        x = cur_freq * cur_pitch
-        instrument.set_freq(x)
-    elif msg.type == 'control_change':
-        if msg.control == 7:
-            #volume knob
-            global_vol = msg.value
-            instrument.vol = global_vol * cur_vol
-        elif msg.control == 3:
-            #next algorithm (ff on my keyboard)
-            if msg.value != 0:
-                instrument.algorithm = (instrument.algorithm + 1) % 9
-                print(instrument.algorithm)
-        elif msg.control == 2:
-            #prev algorithm (rw on my keyboard)
-            if msg.value != 0:
-                instrument.algorithm = (instrument.algorithm - 1) % 9
-                print(instrument.algorithm)
-        elif msg.control == 1:
-            #feedback amount on op[0] - mod wheel
-            t = (msg.value - 64) * (512 / np.pi)
-            instrument.ops[0].modulation = (msg.value - 64) * (8192 / np.pi)
+class PySynth(QRunnable):
+
+    def __init__(self):
+        super(PySynth, self).__init__()
+
+        self.gui_signals = Signaller()
+        self.instrument = Synth()
+        self.p = pyaudio.PyAudio()
+        mido.set_backend('mido.backends.rtmidi/LINUX_ALSA')
+
+        # VARIABLES:
+        self.cur_note = 0
+        self.cur_freq = 0
+        self.cur_vol = 0
+        self.cur_pitch = 1
+        self.global_vol = 128
+
+        self.stream = self.p.open(format=pyaudio.paInt16,
+                                  channels=1,
+                                  rate=48000,
+                                  output=True,
+                                  stream_callback=self.audio_callback)
+        self.stream.start_stream()
+
+        self.inport = mido.open_input(mido.get_input_names()[0],
+                                      callback=self.midi_callback)
+
+    def audio_callback(self, in_data, frame_count, time_info, status):
+        data = np.asarray(self.instrument.get_samples(frame_count), dtype=np.int16)
+        return data, pyaudio.paContinue
+
+    def midi_callback(self, msg):
+        if msg.type == 'note_on':
+            self.cur_note = msg.note
+            self.instrument.press()
+            self.cur_freq = (2 ** ((self.cur_note - 69) / 12)) * 440
+            self.instrument.set_freq(self.cur_freq * self.cur_pitch)
+            self.cur_vol = msg.velocity
+            self.instrument.vol = self.cur_vol * self.global_vol
+        elif msg.type == 'note_off':
+            if msg.note == self.cur_note:
+                self.instrument.release()
+        elif msg.type == 'pitchwheel':
+            # pitch bend - 1 octave range
+            self.cur_pitch = 2 ** (msg.pitch / 8192)
+            x = self.cur_freq * self.cur_pitch
+            print(x)
+            self.instrument.set_freq(x)
+        elif msg.type == 'control_change':
+            if msg.control == 7:
+                # volume knob
+                self.global_vol = msg.value
+                self.instrument.vol = self.global_vol * self.cur_vol
+            elif msg.control == 3:
+                # next algorithm (ff on my keyboard)
+                if msg.value != 0:
+                    self.instrument.algorithm = (self.instrument.algorithm + 1) % 9
+                    print(self.instrument.algorithm)
+            elif msg.control == 2:
+                # prev algorithm (rw on my keyboard)
+                if msg.value != 0:
+                    self.instrument.algorithm = (self.instrument.algorithm - 1) % 9
+                    print(self.instrument.algorithm)
+            elif msg.control == 1:
+                # feedback amount on op[0] - mod wheel
+                t = (msg.value - 64) / 16
+                self.instrument.set_mod(t, 0)
         else:
             print(msg)
-    else:
-        print(msg)
-stream.stop_stream()
-stream.close()
 
-p.terminate()
+    def shutdown(self):
+        self.inport.close()
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = GUI()
+    sys.exit(app.exec_())
