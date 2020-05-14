8e29310a3920a68b2450d146ae15609edc856d2f
sciwizard <audio.sauce@gmail.com>
2020-05-14 16:41:59 -0700

    GUI update: working volume knob.

--- gui.py
+++ gui.py
@@ -16,6 +16,18 @@ class GUI(QWidget):
         self.width = 640
         self.height = 480
 
+        # QObject Inits and Connections
+
+        # Volume control slider
+        self.vol_label = QLabel("Volume")
+        self.vol_label.setAlignment(Qt.AlignCenter)
+        self.vol_slider = QDial()
+        self.vol_slider.setNotchesVisible(True)
+        self.vol_slider.setRange(0, 128)
+        self.vol_slider.setValue(100)
+        self.vol_slider.valueChanged.connect(self.volume_change)
+        self.vol_label.setBuddy(self.vol_slider)
+
         # Objects and inits
         self.synth_signals = Signaller()
         self.synth = PySynth()
@@ -31,26 +43,25 @@ class GUI(QWidget):
         label = QLabel(self)
         pixmap = QPixmap('synthwave.jpg')
         label.setPixmap(pixmap)
-        self.resize(pixmap.width(), pixmap.height())
 
         # Instrument selector
         inst_select = QComboBox()
         inst_select.addItem('Synth 1')
         inst_select.addItem('Synth 2')
 
-        # Volume control slider
-        vol_slider = QSlider(Qt.Vertical)
-
         # Set up the widget grid layout
         layout.addWidget(label, 0, 0)
         layout.addWidget(inst_select, 1, 0)
-        layout.addWidget(vol_slider, 2, 0)
+        layout.addWidget(self.vol_label, 2, 0)
+        layout.addWidget(self.vol_slider, 3, 0)
         self.setLayout(layout)
 
         self.show()
 
-    # Overrides default close event signal
-    # to run the MIDI/audio stream shutdown sequence
+    def volume_change(self):
+        self.synth.global_vol = self.vol_slider.value()
+
+    # Overrides default close event signal to run the MIDI/audio stream shutdown sequence
     def closeEvent(self, event):
         self.synth.shutdown()
         event.accept()
